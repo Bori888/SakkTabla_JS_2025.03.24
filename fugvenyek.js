46105ea226e916c4a7c600c9a737d3dc5a160244
Bori888 <ilyes.bori8@gmail.com>
2025-03-04 21:14:12 +0100

    TABLAgriddel rendezve

--- fugvenyek.js
+++ fugvenyek.js
@@ -1,12 +1,14 @@
 export function sakkTablaKeszites() {
     const TAROLO_ELEM = document.querySelector(".tarolo");
-    for (let index = 0; index < 9; index++) {
-        TAROLO_ELEM.innerHTML += "<div class ='vilagosOldal'></div>";
-        
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+
+            if ((i + j) % 2 === 0) {
+                TAROLO_ELEM.innerHTML += "<div class='vilagosOldal'></div>";
+            } else {
+                TAROLO_ELEM.innerHTML += "<div class='sotetOldal'></div>";
+            }
+        }
     }
-    for (let j = 0; j < 9; j++) {
-        TAROLO_ELEM.innerHTML += "<div class ='sotetOldal'></div>";
-    }
-    
-    
 }
